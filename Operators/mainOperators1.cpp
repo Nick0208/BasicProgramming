a20a22fd1b70adb403291ec3f199c8a8fad7ebda
Nick0208 <160760231+Nick0208@users.noreply.github.com>
2024-02-27 23:25:14 -0500

    Add files via upload

---
+++ Operators/mainOperators1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	int sum1 = 235 + 68;       
+	int sum2 = sum1 + 234;      
+	int sum3 = sum2 + sum2;    
+	
+	cout<<sum3;
+	
+
+	return 0;
+}
